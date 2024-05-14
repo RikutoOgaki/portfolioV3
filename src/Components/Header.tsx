079d3c7efe35b522deca66b05923173685ac767c
Rikuto <orikutobasuke@gmail.com>
2024-05-15 00:31:12 +0900

    HeaderComponent修正

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -8,6 +8,7 @@ type headerData = Array<string>
 
 export function Header() {
 
+    // リンク先名データ
     const [headerTags, setHeaderTags] = useState<headerData>([
         'TOP',
         'ABOUT',
@@ -15,9 +16,49 @@ export function Header() {
         'WORKS'
     ])
 
+    // Scrollが発生すると消え、止めると表示される
+    const [hidden, setHidden] = useState(false)
+    const [lastScrollY, setLastScrollY] = useState(0)
+    let timeoutId: ReturnType<typeof setTimeout> | undefined
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY
+
+            if (currentScrollY > lastScrollY) {
+                setHidden(true)
+            } else {
+                setHidden(false)
+            }
+
+            setLastScrollY(currentScrollY)
+        }
+
+        const handleScrollStop = () => {
+            setHidden(false)
+        }
+
+        const onScroll = () => {
+            if (timeoutId) {
+                clearTimeout(timeoutId)
+            }
+            handleScroll()
+            timeoutId = setTimeout(handleScrollStop, 1000)
+        }
+
+        window.addEventListener('scroll', onScroll)
+
+        return () => {
+            window.removeEventListener('scroll', onScroll)
+            if (timeoutId) {
+                clearTimeout(timeoutId)
+            }
+        }
+    }, [])
+
     return (
         <>
-            <header className={style.headerWrap}>
+            <header className={`${style.headerWrap} ${hidden ? style.headerWrapHidden : ''}`}>
                 <nav className={style.navigationBox}>
                     <ul className={style.navigationSubBox}>
                         {headerTags.map((v, idx) =>

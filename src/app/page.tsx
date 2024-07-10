9079bd24f85e1601cc012c60f7c4033a4fcd0715
Rikuto <orikutobasuke@gmail.com>
2024-07-10 11:57:15 +0900

    修正。Aboutの仮完成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@
 
 import style from '@/styles/page.module.scss'
 
-import { Header } from '@/Components/Header'
-import { MyName } from '@/Components/MyName'
-import { About } from '@/Components/About'
+import { Header } from '@/Components/template/Header'
+import { MyName } from '@/Components/common/MyName'
+import { About } from '@/Components/common/About'
 
 export default function Home() {
   return (
